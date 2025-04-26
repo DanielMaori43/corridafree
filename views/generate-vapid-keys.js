ee2d8d5aea3ad9916abc8351a3069973ec0b9cfa
DanielMaori43 <projetositeviagens@gmail.com>
2025-04-25 21:26:05 -0300

    Delete views/generate-vapid-keys.js

--- views/generate-vapid-keys.js
+++
@@ -1,8 +0,0 @@
-
-const webpush = require('web-push');
-
-// Gerar as chaves VAPID
-const vapidKeys = webpush.generateVAPIDKeys();
-
-// Exibir as chaves no console para que você as copie
-console.log(vapidKeys);
