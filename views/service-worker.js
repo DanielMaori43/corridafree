c6a01bd9a5defde65fe4dacf76e8c492c0e9a9be
DanielMaori43 <projetositeviagens@gmail.com>
2025-04-25 21:00:07 -0300

    Update service-worker.js

--- views/service-worker.js
+++ views/service-worker.js
@@ -1,1 +1,11 @@
+self.addEventListener('push', function(event) {
+  const options = {
+    body: event.data ? event.data.text() : 'Nova notificação',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
 
+  event.waitUntil(
+    self.registration.showNotification('Alerta de Caminhada', options)
+  );
+});
